655068f5d4bf915cf6b0c726b18d62621bc8ba13
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-11-17 15:04:07 +0200

    Styling input fields and adding labels

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,12 +2,26 @@ export default function Meme() {
     return (
         <main>
             <form className="form">
-                <input className="form--input" type="text" placeholder="Top text"></input>
-                <input className="form--input" type="text" placeholder="Bottom text"></input> 
+                <div>
+                    <label htmlFor="top-text">Top Text</label>
+                    <input 
+                        id="top-text" 
+                        className="form--input" 
+                        type="text" 
+                        placeholder="Shut up"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="bottom-text">Bottom Text</label>
+                    <input 
+                        id="bottom-text" 
+                        className="form--input" 
+                        type="text" 
+                        placeholder="and take my money"
+                    />
+                </div>
                 <button className="form--button">Get a new meme image 🖼️</button>
             </form>
         </main>
     )
 }
-
-         
