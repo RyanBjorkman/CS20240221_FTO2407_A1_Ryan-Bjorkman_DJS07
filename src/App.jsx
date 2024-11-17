9933e3bfb887725749dd6c4af140d270709560f4
Ryan Bjorkman <ryanbjorkman@icloud.com>
2024-11-17 14:33:37 +0200

    Created Meme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header" // Import Header component
+import Meme from "./components/Meme" // Import Meme component
 
 function App() {
   return (
     <>
       <Header /> {/* Render the Header component */}
+      <Meme /> {/* Render the Meme component */}
     </>
   )
 }
---
+++ src/components/Meme.jsx
@@ -0,0 +1,13 @@
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input className="form--input" type="text" placeholder="Top text"></input>
+                <input className="form--input" type="text" placeholder="Bottom text"></input> 
+                <button className="form--button">Get a new meme image 🖼️</button>
+            </form>
+        </main>
+    )
+}
+
+         
